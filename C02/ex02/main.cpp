337aa7478658e6ec08bdc785539c61114558ae31
JohannaGcd <guacidejohanna@gmail.com>
2026-03-26 16:15:11 +0100

    ex02 completed, output matches subject

--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -1,30 +1,17 @@
 #include "Fixed.hpp"
-#include <cassert>
 
 int main(void) {
-	Fixed a(6.0f);
-	Fixed b(0.0f);
-	Fixed c(1);
 
-	// assert(a == b);
-	// assert(!(a < b));
-	// assert(!(a > b));
-	// assert(a <= b);
-	// assert(a >= b);
-	// assert(!(a != b));
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
 
-	//assert(a < c);
-	// assert(c > a);
-	// assert(a != c);
-	// assert(a <= c);
-	// assert(c >= a);
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-	//std::cout << a.operator*(b) << std::endl;
-	// std::cout << a.operator+(b) << std::endl;
-	// std::cout << a.operator-(b) << std::endl;
-	std::cout << a.operator/(b) << std::endl;
+	std::cout << b << std::endl;
 
-
-	std::cout << "All comparison tests passed." << std::endl;
-	return 0;
+	std::cout << Fixed::max(a, b) << std::endl;
 }
