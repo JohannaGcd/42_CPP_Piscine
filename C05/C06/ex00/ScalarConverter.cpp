33bebb219e89850d9c9b1f4786d186ae22ce6fa1
JohannaGcd <guacidejohanna@gmail.com>
2026-05-18 17:12:38 +0200

    progress - bare bones C06 - ex00

---
+++ C05/C06/ex00/ScalarConverter.cpp
@@ -0,0 +1,11 @@
+#include "ScalarConverter.hpp"
+#include <iostream>
+
+ScalarConverter::ScalarConverter(){};
+
+
+// Recognize this is an int literal, parse it to get the number 42, then show what 42 looks like as a char, int, float, and double
+
+void ScalarConverter::convert(const std::string str) const {
+	
+}
---
+++ C05/C06/ex00/ScalarConverter.hpp
@@ -0,0 +1,14 @@
+#ifndef SCALAR_CONVERTER_H
+ # define SCALAR_CONVERTER_H
+
+ #include <string>
+
+class ScalarConverter {
+
+	public:
+		void convert(const std::string str) const;
+
+		ScalarConverter();
+};
+
+#endif
