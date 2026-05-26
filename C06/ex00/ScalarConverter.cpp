ad9dbf21f60e23d8128606946b2b030d70b1bd85
JohannaGcd <guacidejohanna@gmail.com>
2026-05-26 18:02:34 +0200

    progress - recognizes appropriate type (without edge cases and other fallback option)

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -11,29 +11,34 @@
 
 // }
 
-ScalarConverter::ScalarConverter(){};
+ScalarConverter::ScalarConverter() {};
 
-std::string recognize_type(const std::string &str) {
-  if (str.size() == 1 && (str[0] > 0 && str[0] < 127))
+std::string recognize_type(const std::string& str) {
+  if (str.size() == 1 &&
+      ((str[0] > 0 && str[0] < 48) || (str[0] > 57 && str[0] < 127)))
     return ("char");
-  else if (str.find('.') && str.find('f'))
-    // check return value of find and update this method
+  else if ((str.find('.') != std::string::npos) &&
+           (str.find('f') != std::string::npos))
     return ("float");
-  else if (str.find('.') && !str.find('f'))
+  else if ((str.find('.') != std::string::npos) &&
+           (str.find('f') == std::string::npos))
     return ("double");
   for (char c : str) {
-    if (c < 48 || c > 57) break;
+    if ((c > 47 && c < 58) || (c == ' ')) break;
     return ("string");
   }
   return ("integer");
   // return ("other");
 }
 
+// void convert_to_type(const std::string& str, const std::string& type) {
+//   if (type == "string") }
+
 // Recognize this is an int literal, parse it to get the number 42,
 //	then show what 42 looks like as a char, int, float, and double
 
-void ScalarConverter::convert(const std::string &str) {
+void ScalarConverter::convert(const std::string& str) {
   std::string type = recognize_type(str);
   std::cout << type << std::endl;
-  // convert_to_type (type);
+  convert_to_type(type);
 }
